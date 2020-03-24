e704cf48ea053b43c1e0e511a153b475ebcf89eb
Ben Pirt <ben@pirt.co.uk>
2020-03-24 16:58:13 +0000

    Re-work to use express

--- lambda.js
+++ lambda.js
@@ -1,57 +1,52 @@
 const authorize = require('./authorize');
 const path = require('path');
 const { loadTemplates, generateRandomString } = require('./lib/utils');
-const { getSession, createSessionCookie } = require('./lib/sessions');
+const { getSession, createSessionToken } = require('./lib/sessions');
 const templates = loadTemplates(path.join(__dirname, './templates'));
+const serverless = require('serverless-http');
+const express = require('express');
+const app = express();
 
-const send = body => {
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'text/html'
-    },
-    body
-  };
-};
+app.get('/dropbox', async (req, res) => {
+  let dropboxId;
+  const session = getSession(req.headers);
 
-const redirect = (url, cookie) => {
-  const result = {
-    statusCode: 302,
-    headers: {
-      'Location': url
-    }
-  };
-  if(cookie) result.headers['Set-Cookie'] = cookie;
+  if(session && session.dropboxId){
+    dropboxId = session.dropboxId;
+  }else{
+    dropboxId = generateRandomString(15);
+    res.cookie('customerToken', createSessionToken(dropboxId), {maxAge: (86400 * 30)});
+  }
+  res.redirect(`dropbox/${dropboxId}`);
+});
+
+app.get('/dropbox/:id', async (req, res) => {
+  const session = getSession(req.headers);
+  if(session && session.dropboxId === req.params.id){
+    // const dropbox = await getDropbox(event.pathParameters.id);
+    const html = templates.userDropboxTemplate({ name: 'Ben' });
+    res.send(html);
+  }else{
+    res.send(400);
+  }
+});
+
+app.post('/dropbox/:id', async (req, res) => {
+  res.redirect(`/dev/dropbox/${req.params.id}`);
+});
+
+const root = async event => {
+  return {statusCode: 302, headers: {'Location': 'dropbox'}};
+}
+
+const authorizer = async event => {
+  const result = await authorize(event);
+  if (result === 'Unauthorized') throw 'Unauthorized';
   return result;
-};
+}
 
 module.exports = {
-  root: async event => {
-    let dropboxId;
-    let cookie = null;
-    const session = getSession(event.headers);
-
-    if(session && session.dropboxId){
-      dropboxId = session.dropboxId;
-    }else{
-      dropboxId = generateRandomString(15);
-      cookie = createSessionCookie(dropboxId)
-    }
-    return redirect(`/dev/dropbox/${dropboxId}`, cookie);
-  },
-
-  getDropbox: async event => {
-    const html = templates.userDropboxTemplate({ name: 'Ben' });
-    return send(html);
-  },
-
-  updateDropbox: async event => {
-    return redirect(`/dev/dropbox/${event.pathParameters.id}`);
-  },
-
-  authorizer: async event => {
-    const result = await authorize(event);
-    if (result === 'Unauthorized') throw 'Unauthorized';
-    return result;
-  }
-};
+  handler: serverless(app),
+  authorizer,
+  root
+}
