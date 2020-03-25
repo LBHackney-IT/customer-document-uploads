1f8e6ddfea5c3bfcff59ab10a772e8e91638ef7a
Ben Pirt <ben@pirt.co.uk>
2020-03-25 14:23:23 +0000

    Rename URLs to dropboxes

--- lambda.js
+++ lambda.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(formidableMiddleware());
 
-app.get('/dropbox', async (req, res) => {
+app.get('/dropboxes/new', async (req, res) => {
   let dropboxId;
   const session = getSession(req.headers);
 
@@ -25,29 +25,29 @@ app.get('/dropbox', async (req, res) => {
       maxAge: 86400 * 30
     });
   }
-  res.redirect(`/${process.env.stage}/dropbox/${dropboxId}`);
+  res.redirect(`/${process.env.stage}/dropboxes/${dropboxId}`);
 });
 
-app.get('/dropbox/:id', async (req, res) => {
+app.get('/dropboxes/:id', async (req, res) => {
   const session = getSession(req.headers);
   if (session && session.dropboxId === req.params.id) {
     const dropbox = await getDropbox(req.params.id);
     const html = templates.userDropboxTemplate({ dropbox });
     res.send(html);
   } else {
-    res.redirect(`/${process.env.stage}/dropbox`);
+    res.redirect(`/${process.env.stage}/dropboxes/new`);
   }
 });
 
-app.post('/dropbox/:id', async (req, res) => {
+app.post('/dropboxes/:id', async (req, res) => {
   await saveDropbox(req.params.id, req.fields, req.files.upload);
-  res.redirect(`/${process.env.stage}/dropbox/${req.params.id}`);
+  res.redirect(`/${process.env.stage}/dropboxes/${req.params.id}`);
 });
 
 const root = async () => {
   return {
     statusCode: 302,
-    headers: { Location: `/${process.env.stage}/dropbox` }
+    headers: { Location: `/${process.env.stage}/dropboxes/new` }
   };
 };
 
