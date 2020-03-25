fbfb09f1203215b5aafca5bb81ae971a0596fa9a
Ben Pirt <ben@pirt.co.uk>
2020-03-25 16:20:52 +0000

    Revert to v5 serverless offline and add admin views

--- lambda.js
+++ lambda.js
@@ -1,53 +1,87 @@
-const authorize = require('./authorize');
+const authorize = require('./lib/authorize');
 const path = require('path');
 const { loadTemplates, generateRandomString } = require('./lib/utils');
 const { getSession, createSessionToken } = require('./lib/sessions');
 const templates = loadTemplates(path.join(__dirname, './templates'));
-const { getDropbox, saveDropbox } = require('./lib/Dependencies');
+const { getDropbox, saveDropbox, getDropboxes } = require('./lib/Dependencies');
 const serverless = require('serverless-http');
 const express = require('express');
 const bodyParser = require('body-parser');
 const formidableMiddleware = require('express-formidable');
 const app = express();
+const globals = {
+  staffLoginUrl: `/dropboxes/`,
+  stage: process.env.stage
+}
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(formidableMiddleware());
 
-app.get('/dropboxes/new', async (req, res) => {
-  let dropboxId;
-  const session = getSession(req.headers);
+app.get('/login', async (req, res) => {
+  const html = templates.loginTemplate({ globals });
+  console.log(res)
+  res.send(html);
+});
 
-  if (session && session.dropboxId) {
-    dropboxId = session.dropboxId;
-  } else {
-    dropboxId = generateRandomString(15);
-    res.cookie('customerToken', createSessionToken(dropboxId), {
-      maxAge: 86400 * 30
-    });
+app.get('/dropboxes', async (req, res) => {
+  if(authorize(req)){
+    const dropboxes = await getDropboxes();
+    const html = templates.staffDropboxListTemplate({ dropboxes, globals });
+    res.send(html);
+  }else{
+    res.redirect(`/login`)
   }
-  res.redirect(`/${process.env.stage}/dropboxes/${dropboxId}`);
+});
+
+app.get('/dropboxes/new', async (req, res) => {
+  if(authorize(req)){
+    res.redirect(`/dropboxes`)
+  }else{
+    let dropboxId;
+    const session = getSession(req.headers);
+
+    if (session && session.dropboxId) {
+      dropboxId = session.dropboxId;
+    } else {
+      dropboxId = generateRandomString(15);
+      res.cookie('customerToken', createSessionToken(dropboxId), {
+        maxAge: 86400 * 30
+      });
+    }
+    res.redirect(`/dropboxes/${dropboxId}`);
+  }
 });
 
 app.get('/dropboxes/:id', async (req, res) => {
   const session = getSession(req.headers);
   if (session && session.dropboxId === req.params.id) {
     const dropbox = await getDropbox(req.params.id);
-    const html = templates.userDropboxTemplate({ dropbox });
+    const html = templates.userDropboxTemplate({ dropbox, globals });
     res.send(html);
   } else {
-    res.redirect(`/${process.env.stage}/dropboxes/new`);
+    res.redirect(`/dropboxes/new`);
+  }
+});
+
+app.get('/dropboxes/:id/view', async (req, res) => {
+  if(authorize(req)){
+    const dropbox = await getDropbox(req.params.id);
+    const html = templates.staffDropboxTemplate({ dropbox, globals });
+    res.send(html);
+  }else{
+    res.redirect(`/login`)
   }
 });
 
 app.post('/dropboxes/:id', async (req, res) => {
   await saveDropbox(req.params.id, req.fields, req.files.upload);
-  res.redirect(`/${process.env.stage}/dropboxes/${req.params.id}`);
+  res.redirect(`/dropboxes/${req.params.id}`);
 });
 
 const root = async () => {
   return {
     statusCode: 302,
-    headers: { Location: `/${process.env.stage}/dropboxes/new` }
+    headers: { Location: `/dropboxes/new` }
   };
 };
 
@@ -59,6 +93,5 @@ const authorizer = async event => {
 
 module.exports = {
   handler: serverless(app),
-  authorizer,
   root
 };
