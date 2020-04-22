8179e54ae4769881a51a6cd61733fc039b227ac4
t-davies <tom.davies@madetech.com>
2020-04-22 16:29:38 +0100

    Adds integration tests for staff dropbox view

--- cypress/integration/staff.spec.js
+++ cypress/integration/staff.spec.js
@@ -1,12 +1,12 @@
 /// <reference types="cypress" />
 
 context('Staff actions', () => {
-  context('given that the user has a token', () => {
-    beforeEach(() => {
-      cy.login();
-      cy.visit('/');
-    });
+  beforeEach(() => {
+    cy.login();
+    cy.visit('/');
+  });
 
+  context('when visiting the service', () => {
     it('shows the staff view', () => {
       cy.get('h2').contains(/customer uploads/i);
     });
@@ -15,6 +15,32 @@ context('Staff actions', () => {
       cy.get('[data-testid=staff-dropboxes-list]')
         .children()
         .should('have.length.greaterThan', 1);
+    });
+  });
+
+  context('when selecting a customer dropbox', () => {
+    it('shows the customer dropbox details', () => {
+      cy.get('[data-testid=staff-dropboxes-list]')
+        .children()
+        .first()
+        .find('[data-testid=dropbox-link]')
+        .click();
+
+      cy.get('[data-testid=dropbox-details]').should('exist');
+    });
+  });
+
+  context('when viewing a customer dropbox', () => {
+    it('can navigate back to the dropboxes list', () => {
+      cy.get('[data-testid=staff-dropboxes-list]')
+        .children()
+        .first()
+        .find('[data-testid=dropbox-link]')
+        .click();
+
+      cy.get('[data-testid=dropbox-list-return-link]').click();
+
+      cy.get('[data-testid=staff-dropboxes-list]').should('exist');
     });
   });
 });
