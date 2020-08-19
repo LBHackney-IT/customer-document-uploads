1d802346e65c58b857c9d9e9096adcd42c30b6e8
Kat <katrina@madetech.com>
2020-08-19 11:38:34 +0100

    Include dotenv in jest tests

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   clearMocks: true,
   testEnvironment: 'node',
   testRegex: '(/__tests__/.*|(\\.|/)(test))\\.[jt]sx?$',
+  setupFiles: ['dotenv/config'],
   setupFilesAfterEnv: ['<rootDir>/setupTests.js']
 };
